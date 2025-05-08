e216acb107cfbfa3acebba89d9ffa424e9361742
MoizAli4444 <moizali1280@gmail.com>
2025-05-09 01:47:17 +0500

    express setup complete

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 
+require('dotenv').config();
+
 // express app
 const app = express();
 
+//middleware
+app.use((req,res,next)=>{
+    console.log(req.path, req.method);
+    next();
+});
+
+// routes
+app.get('/',(req,res)=>{
+    res.json({msg:'welcome to workout project'});
+});
+
 // listen for requests
-app.listen(4000,()=>{
-    console.log('app run on 4000');
+app.listen(process.env.PORT,()=>{
+    console.log('app run on :',process.env.PORT);
 });
