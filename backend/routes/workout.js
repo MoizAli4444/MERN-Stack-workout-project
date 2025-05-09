87b3f1879f1eccd5c1fa65d44fee166099a7f788
MoizAli4444 <moizali1280@gmail.com>
2025-05-09 23:18:50 +0500

    shift route function to controller

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -16,14 +16,7 @@ router.get("/:id", (req,res) => {
 
 // post a new workout
 router.post("/", async (req,res) => {
-    const {title, load, reps} = req.body;
-
-    try {
-        const workout = await Workout.create({title, load, reps});
-        res.status(200).json(workout);
-    } catch (error) {
-        res.status(400).json({error:error.message});
-    }
+    
 });
 
 // delete a workout
