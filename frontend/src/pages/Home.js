9ae6d21056ff9b831d36b9ae53f0f49022a4dc49
MoizAli4444 <moizali1280@gmail.com>
2025-05-17 00:37:13 +0500

    add proxy for backend and load data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,34 +1,32 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
-const Home = () =>{
+const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
 
-    const [workouts, setWorkouts] = useState(null);
+  useEffect(() => {
+    const fetchWorkouts = async () => {
 
-    useEffect(() => {
-      const fetchWorkouts = async ()=>{
-        const response = await fetch ('http://localhost:4000/api/workouts');
-        const json = await response.json();
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
 
-        if (response.ok) {
-            setWorkouts(json);
-        }
-
-        fetchWorkouts();
-
+      if (response.ok) {
+        setWorkouts(json);
       }
-    
-    }, [])
+    };
+    fetchWorkouts();
+  }, []);
 
-    return (
-        <div className="home">
-            <div className='workouts'>
-                {workouts && workouts.map((workout)=>(
-                    <p key={workout._id}> {workout.title} </p>
-                ))}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      <h1>home</h1>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <p key={workout._id}> {workout.title } </p>
+          ))}
+      </div>
+    </div>
+  );
+};
 
-
-export default Home
+export default Home;
