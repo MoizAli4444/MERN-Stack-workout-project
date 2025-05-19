3cdb9b992dd2fc18d7f341b725e87e384b5127a8
MoizAli4444 <moizali1280@gmail.com>
2025-05-19 16:45:13 +0500

    make create and set workouts

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -2,10 +2,29 @@ import { createContext } from "react";
 
 export const WorkoutsContext = createContext();
 
-export const WorkoutsContextProvider = () => {
+
+export const workoutsReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_WORKOUTS':
+            return {
+                workouts:action.payload
+            }
+            
+        case 'CREATE_WORKOUTS':
+            return {
+                workouts:[action.payload,...state.workouts]
+            }
+    
+        default:
+            break;
+    }
+}
+
+
+export const WorkoutsContextProvider = ({children}) => {
     return (
         <WorkoutsContextProvider>
-            
+            {children}
         </WorkoutsContextProvider>
     )
 } 
