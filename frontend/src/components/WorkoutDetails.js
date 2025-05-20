16be1e6cb82a6dfd401c4eac7a698fae10a679dc
MoizAli4444 <moizali1280@gmail.com>
2025-05-20 16:38:13 +0500

    delete code complete

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,6 +1,7 @@
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 const WorkoutDetails = ({ workout }) => {
+  const {dispatch} = useWorkoutsContext();
 
   const handleClick = async () => {
     const response = await fetch('/api/workouts/'+workout._id,{
